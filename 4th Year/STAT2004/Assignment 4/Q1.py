40d77a7e516309196dfaa804c5a166676ba0601e
Ryan White <ryanwhite_1@yahoo.com.au>
2022-10-27 13:30:50 +1000

    Update Q1.py

--- 4th Year/STAT2004/Assignment 4/Q1.py
+++ 4th Year/STAT2004/Assignment 4/Q1.py
@@ -10,8 +10,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
+# q1a
 data = pd.read_csv("likes.csv", delimiter=',')
 
 print('mean = ', np.mean(data['rating']), ' sd = ', np.std(data['rating']), ' median = ', np.median(data['rating']), 
       ' mode = ', stats.mode(data['rating']))
 # plt.hist(data['rating'])
+
+# q2h
+print(1 - stats.chi2.cdf(6.368, 1))
+
+#q2i
+row1 = 52; row2 = 30
+col1 = 41; col2 = 41
+iters = 10**6
+counts = 0
+
+for i in range(iters):
+    x1 = np.random.randint(0, min(row1 + 1, col1))
+    x2 = row1 - x1
+    x3 = col1 - x1
+    x4 = col2 - x2
+    if (x2 - x3)**2 / (x2 + x3) >= 6.368:
+        counts += 1
+
+print(f"p-value from {iters} trials is ", counts / iters)
+
