ccbaaf3b2c8f1bbc62af1bee68551696b05a6e58
Ryan White <ryanwhite_1@yahoo.com.au>
2023-04-15 11:13:33 +1000

    3202 Comm 9

--- 5th Year/MATH3202/Ass 2/Ass2.py
+++ 5th Year/MATH3202/Ass 2/Ass2.py
@@ -16,6 +16,7 @@ pipes = pd.read_csv("pipelines.csv")
 N = range(len(nodes['Node']))
 T = range(2) # number of days we're looking at
 O = range(4) # number of options we have to upgrade (0th option is no upgrade)
+F = range(3) # number of possible demand scenarios in 10 years
 
 
 # E[i,j] gives the distance (km) from node i to node j
@@ -42,48 +43,57 @@ Upgrades = {(20, 0): [0, 0], (27, 0): [0, 0], (36, 0): [0, 0], (45, 0): [0, 0],
     
 Pmax = 358 # MJ, maximum pipeline capacity
 Pcost = 200000 # $/km, upgrade cost of pipeline per km
-CMult = [1, 0.7]
+CMult = [1, 0.7] # Cost multiplier for upgrading things now vs in 5 years
+DMult = [[1, 1, 1], [0.8, 1, 1.2]] # Demand multipliers for forecast demand in 10 years
 
 m = Model("Upgrades")
 
 # Variables
-X = {(n, t): m.addVar() for n in N for t in T}
-Y = {(e, t): m.addVar() for e in E for t in T}
-W = {(o, s, t): m.addVar(vtype=GRB.BINARY) for o in O for s in S for t in T}
-P = {(e, t): m.addVar(vtype=GRB.BINARY) for e in E for t in T}
+X = {(n, t, f): m.addVar() for n in N for t in T for f in F}
+Y = {(e, t, f): m.addVar() for e in E for t in T for f in F}
+W = {(o, s, t, f): m.addVar(vtype=GRB.BINARY) for o in O for s in S for t in T for f in F}
+P = {(e, t, f): m.addVar(vtype=GRB.BINARY) for e in E for t in T for f in F}
 
 # Objective
-m.setObjective(quicksum(CMult[t] * W[o, s, t] * Upgrades[s, o][1] for o in O for s in S for t in T) 
-               + quicksum(CMult[t] * P[e, t] * E[e] * Pcost for e in E for t in T),
+m.setObjective(quicksum(1/3 * (quicksum(CMult[t] * W[o, s, t, f] * Upgrades[s, o][1] for o in O for s in S for t in T) 
+               + quicksum(CMult[t] * P[e, t, f] * E[e] * Pcost for e in E for t in T)) for f in F),
                GRB.MINIMIZE)
 
 # Constraints
-for t in T:
-    for n in N:
-        ## Supply constraints
-        if n in S:
-            s = n
-            # below reads as:   node output <= max_supply + all of the upgrades done so far * the upgrade capacity
-            m.addConstr(X[s, t] <= S[s] + quicksum(W[o, s, j] * Upgrades[s, o][0] for o in O for j in T[:t+1]))
-        else:
-            m.addConstr(X[n, t] <= 0)
-            
-        # Flow constraint:
-        m.addConstr(X[n, t] + quicksum(Y[e, t] for e in E if e[1] == n) == 
-                    quicksum(Y[e, t] for e in E if e[0] == n) + demand[n, years[t]])
+for f in F:
+    for t in T:
+        for n in N:
+            ## Supply constraints
+            if n in S:
+                s = n
+                # below reads as:   node output <= max_supply + all of the upgrades done so far * the upgrade capacity
+                m.addConstr(X[s, t, f] <= S[s] + quicksum(W[o, s, j, f] * Upgrades[s, o][0] for o in O for j in T[:t+1]))
+            else:
+                m.addConstr(X[n, t, f] <= 0)
+                
+            # Flow constraint:
+            m.addConstr(X[n, t, f] + quicksum(Y[e, t, f] for e in E if e[1] == n) == 
+                        quicksum(Y[e, t, f] for e in E if e[0] == n) + DMult[t][f] * demand[n, years[t]])
 
 ## Unique upgrade constraint
-for s in S:
-    m.addConstr(quicksum(W[o, s, t] for o in O for t in T) == 1)
+for f in F:
+    for s in S:
+        m.addConstr(quicksum(W[o, s, t, f] for o in O for t in T) == 1)
 
-## Pipeline upgrade constraint
-for t in T:
-    for e in E:
-        # below reads as:   pipeflow at this time <= max pipeflow + sum of the pipeflow upgrades done so far
-        m.addConstr(Y[e, t] <= Pmax * (1 + quicksum(P[e, j] for j in T[:t+1])))
+## Pipeline upgrade constraints
+for f in F:
+    for t in T:
+        for e in E:
+            # below reads as:   pipeflow at this time <= max pipeflow + sum of the pipeflow upgrades done so far
+            m.addConstr(Y[e, t, f] <= Pmax * (1 + quicksum(P[e, j, f] for j in T[:t+1])))
+    for e in E: # max one upgrade!
+        m.addConstr(quicksum(P[e, t, f] for t in T) <= 1)
 
+## 5-Year Upgrade Constraints
 for e in E:
-    m.addConstr(quicksum(P[e, t] for t in T) <= 1)
+    m.addConstr(quicksum(P[e, 0, f] for f in F) == len(F) * P[e, 0, 0])
+for s in S:
+    m.addConstr(quicksum(W[o, s, 0, f] for f in F for o in O) == len(F) * quicksum(W[o, s, 0, 0] for o in O))
 
 
 # Optimise!
