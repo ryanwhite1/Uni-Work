741c2d54cc3023f7b46e28c07dc21f088dda309f
Ryan White <ryanwhite_1@yahoo.com.au>
2023-05-13 10:28:23 +1000

    3202 Comm 13

--- 5th Year/MATH3202/Ass 3/Ass3.py
+++ 5th Year/MATH3202/Ass 3/Ass3.py
@@ -10,6 +10,8 @@ def deliv_cost(n):
     return 300 + 50 * n
 
 demand = [7, 14, 8, 8, 12, 15, 6, 15, 7, 12, 13, 9, 6, 11]
+high_demand = [12, 17, 17, 12, 18, 22, 14, 19, 11, 20, 19, 18, 13, 20]
+p = 0.4
 r = 120
 cap = 30
 Dcap = 1000
@@ -22,7 +24,9 @@ def gas(t, s):
         if t == 14:
             gas_[t, s] = (0, 'finish')
         else:
-            print(t)
-            gas_[t, s] = max([(min(demand[t], s + n) * r - min(n, 1) * deliv_cost(n) + 
-                               gas(t + 1, s + n - min(demand[t], s + n))[0], n) for n in range(0, min(cap - s + demand[t], Ncyl) + 1)])
+            gas_[t, s] = max([((p * min(high_demand[t], s + n) + (1 - p) * min(demand[t], s + n)) * r - min(n, 1) * deliv_cost(n) + 
+                               gas_stoc(t, s, n), n) for n in range(0, min(cap - s + high_demand[t], Ncyl) + 1)])
     return gas_[t, s]
+
+def gas_stoc(t, s, n):
+    return p * gas(t + 1, s + n - min(high_demand[t], s + n))[0] + (1 - p) * gas(t + 1, min(cap, s + n - min(demand[t], s + n)))[0]
