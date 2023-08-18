268f9f0afa7bead6a08f959e379bd784f8d2e4c2
Ryan White <ryanwhite_1@yahoo.com.au>
2023-08-18 16:25:24 +1000

    3070 assignment 1 finished

--- 5th Year/MATH3070/Assignment 1/Q1.py
+++ 5th Year/MATH3070/Assignment 1/Q1.py
@@ -12,9 +12,13 @@ plt.rcParams.update({"text.usetex": True})
 plt.rcParams['font.family']='serif'
 plt.rcParams['mathtext.fontset']='cm'
 
+
+
+### Q1d ###
 def model(x, k, r):
     return r * x * np.exp(- np.log(r) * x / k)
 
+# initialise variables and parameters of interest
 x0 = 0.1
 k = 1
 rs = np.linspace(0.001, 35, 500)
@@ -30,10 +34,10 @@ pops[:, 0] = x0
 for i, r in enumerate(rs):
     for j, t in enumerate(times):
         if j != 0:
-            pops[i, j] = model(pops[i, j - 1], k, r)
+            pops[i, j] = model(pops[i, j - 1], k, r) # calculate population for this time, based on the previous time population
       
 for i in range(1000, 1500):
-    ax.scatter(rs, pops[:, i] / k, s=0.01, c='tab:blue')
+    ax.scatter(rs, pops[:, i] / k, s=0.01, c='tab:blue') # plot the late time pops as single markers
     
 ax.set_xlabel("Proliferation Value, $r$")
 ax.set_ylabel("Late time population (prop. of $k$)")
@@ -42,7 +46,7 @@ fig.savefig('Q1d.pdf', dpi=400, bbox_inches='tight')
 
 
 ### Q1e ###
-
+# analogous to 1d code above
 def modelH(x, k, r):
     if x < k / r:
         return r * x
@@ -73,3 +77,41 @@ ax.set_xlabel("Proliferation Value, $r$")
 ax.set_ylabel("Late time population (prop. of $k$)")
 fig.savefig('Q1e.png', dpi=400, bbox_inches='tight')
 fig.savefig('Q1e.pdf', dpi=400, bbox_inches='tight')
+
+
+
+### Q1g ###
+
+from scipy.optimize import brentq # bisection function in python
+
+def rx_deriv_root(s, r, k):
+    # corresponds to R'(S) - 1 = 0
+    return r * np.exp(-np.log(r) * s / k) * (1 - np.log(r) * s / k) - 1 
+k = 1
+r = 2
+
+root = brentq(rx_deriv_root, 0.0001, k, args=(r, k)) # find the root
+print(f"Q1g root is S = {root}")
+
+
+### Q1h ###
+
+rs = np.linspace(1, 3, 1000)
+k = 1 
+
+R_escape = np.zeros(len(rs))
+H_escape = k / rs # can just calculate the escapement for H
+
+for i, r in enumerate(rs):
+    R_escape[i] = brentq(rx_deriv_root, 0.0001, k, args=(r, k)) # find the root for this proliferation
+
+fig, ax = plt.subplots(figsize=(8, 4))
+
+ax.plot(rs, H_escape, ls='--', c='k', label='$H(X_t - h_t)$')
+ax.plot(rs, R_escape, c='tab:red', label='$R(X_t - h_t)$')
+ax.legend()
+ax.set_xlabel("Proliferation, $r$")
+ax.set_ylabel("Optimal Escapement (prop. of $k$), $S$")
+
+fig.savefig('Q1h.png', dpi=400, bbox_inches='tight')
+fig.savefig('Q1h.pdf', dpi=400, bbox_inches='tight')
--- 5th Year/MATH3070/Assignment 1/Q2.py
+++ 5th Year/MATH3070/Assignment 1/Q2.py
@@ -7,39 +7,35 @@ Created on Fri Aug 11 22:47:46 2023
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import odeint
 
 plt.rcParams.update({"text.usetex": True})
 plt.rcParams['font.family']='serif'
 plt.rcParams['mathtext.fontset']='cm'
 
-def dxdt(x, t, m, h):
-    return x * (1 - x) + m - h * x
-
 def equilibria(h, m):
+    # returns each equilibrium for a given h and m 
     a = 0.5 * (1 - h)
     b = 0.5 * np.sqrt(1 - 2 * h + h**2 + 4 * m)
     return [a - b, a + b]
 
-x0 = 1
-
-ms = [-0.02, 0, 0.02]
-times = np.linspace(0, 1500, 1500)
-dt = times[1] - times[0]
+ms = [-0.02, 0, 0.02] # m parameters of interest
+times = np.linspace(0, 1500, 1500) 
 hs = np.linspace(0, 1, 500)
 
 fig, axes = plt.subplots(nrows=len(ms), sharex=True)
 fig.subplots_adjust(hspace=0)
 
 for i, m in enumerate(ms):
-    top = [equilibria(h, m)[1] for h in hs]
-    bottom = [equilibria(h, m)[0] for h in hs]
+    top = [equilibria(h, m)[1] for h in hs] # get top equilibria
+    bottom = [equilibria(h, m)[0] for h in hs] # get bottom equilibria 
+    # now plot each of them as a point on the plot
     axes[i].scatter(hs, top, s=0.1, c='tab:blue', label=f"$m={m}$")
     axes[i].scatter(hs, bottom, s=0.1, c='tab:blue')
     axes[i].legend()
     axes[i].set_ylim(-0.25, 1)
     axes[i].set_xlim(0, 1)
 
+# now play around with the labels a bit to make it pretty
 for i, ax in enumerate(axes):
     labels = ax.get_yticklabels()
     if i in [0, 1]:
