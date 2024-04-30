6b6c2875c273f276eec2bad59b99fbedb32b4dfa
Ryan White <ryanwhite_1@yahoo.com.au>
2024-04-30 16:52:53 +1000

    broken gif code of orbits with plume

--- Honours/Talks/Honours Progress Talk/ModelPlot.py
+++ Honours/Talks/Honours Progress Talk/ModelPlot.py
@@ -311,7 +311,7 @@ def dust_plume_sub(theta, times, n_orbits, period_s, stardata):
                             jnp.zeros(n_time)])
     positions2 = jnp.copy(positions1)
     positions1 *= -r1      # position in the orbital frame
-    positions2 *=  r2     # position in the orbital frame
+    positions2 *= r2     # position in the orbital frame
     
     widths = stardata['windspeed1'] * period_s * (n_orbits - jnp.arange(n_time) / n_t)
     
@@ -329,7 +329,7 @@ def dust_plume_sub(theta, times, n_orbits, period_s, stardata):
                            jnp.ravel(particles[:, 2, :])])
     
 
-    shock_start = plume_direction * (1 + stardata['nuc_dist'] * AU2km / jnp.max(jnp.abs(plume_direction)))
+    shock_start = plume_direction * (1 + stardata['nuc_dist'] * AU2km / jnp.max(jnp.abs(plume_direction))) + positions2
     shock_start = jnp.repeat(shock_start, len(theta), axis=1)
     particles += shock_start
 
@@ -353,6 +353,26 @@ def dust_plume(stardata):
     n_orbits = 1
     n_t = 30       # circles per orbital period
     n_points = 40   # points per circle
+    n_particles = n_points * n_t * n_orbits
+    n_time = n_t * n_orbits
+    theta = 2 * jnp.pi * jnp.linspace(0, 1, n_points)
+    times = period_s * jnp.linspace(phase, n_orbits + phase, n_time)
+    particles, weights = dust_plume_sub(theta, times, n_orbits, period_s, stardata)
+    return particles, weights
+@jit
+def dust_plume_for_gif(stardata):
+    '''
+    Parameters
+    ----------
+    stardata : dict
+    '''
+    phase = stardata['phase']%1
+    
+    period_s = stardata['period'] * 365.25 * 24 * 60 * 60
+    
+    n_orbits = 1
+    n_t = 1000       # circles per orbital period
+    n_points = 400   # points per circle
     n_particles = n_points * n_t * n_orbits
     n_time = n_t * n_orbits
     theta = 2 * jnp.pi * jnp.linspace(0, 1, n_points)
@@ -647,58 +667,184 @@ test_system = {"m1":15.,                # solar masses
         "sigma":5.,              # sigma for gaussian blur
         "histmax":1.}
 
-# # for i in range(10):
-t1 = time.time()
-particles, weights = dust_plume(test_system)
-X, Y, H = smooth_histogram2d(particles, weights, test_system)
-print(time.time() - t1)
-plot_spiral(X, Y, H)
+# # # for i in range(10):
+# t1 = time.time()
+# particles, weights = dust_plume(test_system)
+# X, Y, H = smooth_histogram2d(particles, weights, test_system)
+# print(time.time() - t1)
+# plot_spiral(X, Y, H)
 
-plot_3d(particles, weights)
-
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-def rotate_x(angle):
-    arr = np.array([[1, 0, 0],
-                     [0, np.cos(angle), np.sin(angle)],
-                     [0, -np.sin(angle), np.cos(angle)]])
-    return arr
-
-def rotate_y(angle):
-    arr = np.array([[np.cos(angle), 0, -np.sin(angle)],
-                     [0, 1, 0],
-                     [np.sin(angle), 0, np.cos(angle)]])
-    return arr
-
-def rotate_z(angle):
-    arr = np.array([[np.cos(angle), np.sin(angle), 0],
-                     [-np.sin(angle), np.cos(angle), 0],
-                     [0, 0, 1]])
-    return arr
+# plot_3d(particles, weights)
 
 
-theta = np.linspace(0, 2*np.pi, 30)
-phi = np.pi / 2
-r = 1
 
-z = r * np.cos(theta) * np.sin(phi)
-y = r * np.sin(theta) * np.sin(phi)
-x = r * np.cos(phi) + 1 * np.ones(len(theta))
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
+# def rotate_x(angle):
+#     arr = np.array([[1, 0, 0],
+#                      [0, np.cos(angle), np.sin(angle)],
+#                      [0, -np.sin(angle), np.cos(angle)]])
+#     return arr
 
-ax.plot3D(x, y, z, marker='.', ls='')
-ax.set(xlabel='', ylabel='', zlabel='', 
-       xticklabels=[], yticklabels=[], zticklabels=[],
-       xlim=(0.5, 3))
-ax.set_box_aspect([1.0, 1.0, 1.0])
-ax.view_init(elev=30, azim=40)
-fig.savefig('RingStructure.png', dpi=400, bbox_inches='tight')
+# def rotate_y(angle):
+#     arr = np.array([[np.cos(angle), 0, -np.sin(angle)],
+#                      [0, 1, 0],
+#                      [np.sin(angle), 0, np.cos(angle)]])
+#     return arr
+
+# def rotate_z(angle):
+#     arr = np.array([[np.cos(angle), np.sin(angle), 0],
+#                      [-np.sin(angle), np.cos(angle), 0],
+#                      [0, 0, 1]])
+#     return arr
+
+
+# theta = np.linspace(0, 2*np.pi, 30)
+# phi = np.pi / 2
+# r = 1
+
+# z = r * np.cos(theta) * np.sin(phi)
+# y = r * np.sin(theta) * np.sin(phi)
+# x = r * np.cos(phi) + 1 * np.ones(len(theta))
+
+# fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')
+
+# ax.plot3D(x, y, z, marker='.', ls='')
+# ax.set(xlabel='', ylabel='', zlabel='', 
+#        xticklabels=[], yticklabels=[], zticklabels=[],
+#        xlim=(0.5, 3))
+# ax.set_box_aspect([1.0, 1.0, 1.0])
+# ax.view_init(elev=30, azim=40)
+# fig.savefig('RingStructure.png', dpi=400, bbox_inches='tight')
+
+
+
+
+def orbital_positions(stardata):
     
+    phase = stardata['phase']%1
+    
+    period_s = stardata['period'] * 365.25 * 24 * 60 * 60
+    
+    n_orbits = 1
+    n_t = 100       # circles per orbital period
+    n_points = 40   # points per circle
+    n_particles = n_points * n_t * n_orbits
+    n_time = n_t * n_orbits
+    theta = 2 * jnp.pi * jnp.linspace(0, 1, n_points)
+    times = period_s * jnp.linspace(phase, n_orbits + phase, n_time)
+    n_time = len(times)
+    n_t = n_time / n_orbits
+    ecc = stardata['eccentricity']
+    # E, true_anomaly = kepler_solve(times, period_s, ecc)
+    
+    E, true_anomaly = kepler(2 * jnp.pi * times / period_s, jnp.array([ecc]))
+    
+    a1, a2 = calculate_semi_major(period_s, stardata['m1'], stardata['m2'])
+    r1 = a1 * (1 - ecc * jnp.cos(E)) * 1e-3     # radius in km 
+    r2 = a2 * (1 - ecc * jnp.cos(E)) * 1e-3
+    # ws_ratio = stardata['windspeed1'] / stardata['windspeed2']
+    
+    positions1 = jnp.array([jnp.cos(true_anomaly), 
+                            jnp.sin(true_anomaly), 
+                            jnp.zeros(n_time)])
+    positions2 = jnp.copy(positions1)
+    positions1 *= r1      # position in the orbital frame
+    positions2 *= -r2     # position in the orbital frame
+    
+    return positions1, positions2
+
+def transform_orbits(pos1, pos2, stardata):
+    pos1 = rotate_z(jnp.deg2rad(- stardata['asc_node'])) @ (
+            rotate_x(jnp.deg2rad(- stardata['inclination'])) @ (
+            rotate_z(jnp.deg2rad(- stardata['arg_peri'])) @ pos1))
+    pos2 = rotate_z(jnp.deg2rad(- stardata['asc_node'])) @ (
+            rotate_x(jnp.deg2rad(- stardata['inclination'])) @ (
+            rotate_z(jnp.deg2rad(- stardata['arg_peri'])) @ pos2))
+    pos1 = 60 * 60 * 180 / jnp.pi * jnp.arctan(pos1 / (stardata['distance'] * 3.086e13))
+    pos2 = 60 * 60 * 180 / jnp.pi * jnp.arctan(pos2 / (stardata['distance'] * 3.086e13))
+    return pos1, pos2
+
+# @jit
+def orbit_spiral_gif(stardata):
+    '''
+    '''
+    starcopy = stardata.copy()
+    fig, ax = plt.subplots()
+    
+    every = 1
+    length = 10
+    # now calculate some parameters for the animation frames and timing
+    # nt = int(stardata['period'])    # roughly one year per frame
+    nt = 50
+    # nt = 10
+    frames = jnp.arange(0, nt, every)    # iterable for the animation function. Chooses which frames (indices) to animate.
+    fps = len(frames) // length  # fps for the final animation
+    
+    phases = jnp.linspace(0, 1, nt)
+    pos1, pos2 = orbital_positions(test_system)
+    pos1, pos2 = transform_orbits(pos1, pos2, stardata)
+    
+    
+    lim = 2 * max(np.max(np.abs(pos1)), np.max(np.abs(pos1)))
+    xbins = np.linspace(-lim, lim, 257)
+    ybins = np.linspace(-lim, lim, 257)
+    
+    # @jit
+    def animate(i):
+        ax.cla()
+        print(i)
+        starcopy['phase'] = phases[i] + 0.5
+        particles, weights = dust_plume_for_gif(starcopy)
+        particles /= 1e2
+        
+        pos1, pos2 = orbital_positions(starcopy)
+        pos1, pos2 = transform_orbits(pos1, pos2, starcopy)
+        
+        X, Y, H = spiral_grid_w_bins(particles, weights, starcopy, xbins, ybins)
+        ax.pcolormesh(X, Y, H, cmap='hot')
+        # ax.pcolormesh(X+pos2[0, -1], Y+pos2[1, -1], H, cmap='hot')
+        
+        
+        ax.plot(pos1[0, :], pos1[1, :], c='w')
+        ax.plot(pos2[0, :], pos2[1, :], c='w')
+        ax.scatter([pos1[0, -1], pos2[0, -1]], [pos1[1, -1], pos2[1, -1]], c=['tab:cyan', 'w'], s=100)
+        
+        ax.set(xlim=(-lim, lim), ylim=(-lim, lim))
+        ax.set_facecolor('k')
+        return fig, 
+
+    ani = animation.FuncAnimation(fig, animate, frames=frames, blit=True, repeat=False)
+    ani.save(f"orbit_spiral.gif", writer='pillow', fps=fps)
+
+test_system = {"m1":22.,                # solar masses
+        "m2":10.,                # solar masses
+        "eccentricity":0.6, 
+        "inclination":5.,       # degrees
+        "asc_node":254.1,         # degrees
+        "arg_peri":10.6,           # degrees
+        "open_angle":40.,       # degrees (full opening angle)
+        "period":1.,           # years
+        "distance":10.,        # pc
+        "windspeed1":10.,       # km/s
+        "windspeed2":2400.,      # km/s
+        "turn_on":-150.,         # true anomaly (degrees)
+        "turn_off":150.,         # true anomaly (degrees)
+        "oblate":0.,
+        "nuc_dist":0.0001, "opt_thin_dist":2.,           # nucleation and optically thin distance (AU)
+        "acc_max":0.1,                                 # maximum acceleration (km/s/yr)
+        "orb_sd":0., "orb_amp":0., "orb_min":180., "az_sd":30., "az_amp":0., "az_min":270.,
+        "comp_incl":127.1, "comp_az":116.5, "comp_open":0., "comp_reduction":0., "comp_plume":1.,
+        "phase":0.6, 
+        "sigma":5.,              # sigma for gaussian blur
+        "histmax":1.}
+orbit_spiral_gif(test_system)
+    
+    
+
+
 
 
 
